24f92d1db3c648222b577233461ae445cfbd9e99
Bo Andersen <ba0708@icloud.com>
2017-07-13 13:07:48 +0200

    Increased quantity when adding same product

--- js/app.js
+++ js/app.js
@@ -52,12 +52,27 @@ new Vue({
     },
     methods: {
         addProductToCart: function(product) {
-            this.cart.items.push({
-                product: product,
-                quantity: 1
-            });
+            var cartItem = this.getCartItem(product);
+        
+            if (cartItem != null) {
+                cartItem.quantity++;
+            } else {
+                this.cart.items.push({
+                    product: product,
+                    quantity: 1
+                });
+            }
         
             product.inStock--;
+        },
+        getCartItem: function(product) {
+            for (var i = 0; i < this.cart.items.length; i++) {
+                if (this.cart.items[i].product.id === product.id) {
+                    return this.cart.items[i];
+                }
+            }
+        
+            return null;
         }
     },
     computed: {
