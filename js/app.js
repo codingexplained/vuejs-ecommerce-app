84043496fd822e19af97c1c6acac853cb313a61e
Bo Andersen <ba0708@icloud.com>
2017-07-13 12:43:39 +0200

    Displayed products

--- js/app.js
+++ js/app.js
@@ -45,5 +45,16 @@ new Vue({
                 inStock: 81
             }
         ]
+    },
+    filters: {
+        currency: function(value) {
+            var formatter = new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 0
+            });
+            
+            return formatter.format(value);
+        }
     }
 });
