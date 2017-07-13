f12725ecdcba3de6c9f8c50a48dd5824c1ddd5d9
Bo Andersen <ba0708@icloud.com>
2017-07-13 12:38:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vue.js E-commerce Example Application
+
+This repository contains the code for the first example application from the [Vue.js: From Beginner to Professional course](https://codingexplained.com/l/github/vue-js-github).
+
+Note that this application is **not intended for showing best practices**, as it is the very first application in a large Vue.js course.
---
+++ js/app.js
@@ -0,0 +1,49 @@
+new Vue({
+    el: '#app',
+    data: {
+        products: [
+            {
+                id: 1,
+                name: 'MacBook Pro (15 inch)',
+                description: 'This laptop has a super crisp Retina display. Yes, we know that it\'s overpriced...',
+                price: 2999,
+                inStock: 50
+            },
+            {
+                id: 2,
+                name: 'Samsung Galaxy Note 7',
+                description: 'Unlike the overpriced MacBook Pro, we\'re selling this one a bit cheap, as we heard it might explode...',
+                price: 299,
+                inStock: 755
+            },
+            {
+                id: 3,
+                name: 'HP Officejet 5740 e-All-in-One-printer',
+                description: 'This one might not last for so long, but hey, printers never work anyways, right?',
+                price: 149,
+                inStock: 5
+            },
+            {
+                id: 4,
+                name: 'iPhone 7 cover',
+                description: 'Having problems keeping a hold of that phone, huh? Ever considered not dropping it in the first place?',
+                price: 49,
+                inStock: 42
+            },
+            {
+                id: 5,
+                name: 'iPad Pro (9.7 inch)',
+                description: 'We heard it\'s supposed to be pretty good. At least that\'s what people say.',
+                price: 599,
+                inStock: 0
+            },
+            {
+                id: 6,
+                name: 'OnePlus 3 cover',
+                description: 'Does your phone spend most of its time on the ground? This cheap piece of plastic is the solution!',
+                price: 19,
+                inStock: 81
+            }
+        ]
+    }
+});
